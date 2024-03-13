4d4ca18ba961fd0326fd78d51e362eec2d36d0b4
Olubodun <bolowys33@gmail.com>
2024-03-13 20:45:09 +0100

    Add EmptyCart component

---
+++ src/components/EmptyCart.jsx
@@ -0,0 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import basket from '../assets/emptycart.png'
+
+const EmptyCart = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(`/products`);
+    };
+
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <div className="inset-0 flex items-center justify-center bg-slate-100">
+                <div
+                    className="bg-white p-6 rounded-lg text-center shadow-lg">
+                    <img
+                        src={basket}
+                        alt="Empty Cart"
+                        style={{
+                            display: "block",
+                            margin: "0 auto",
+                            width: "80px",
+                        }}
+                    />
+                    <h1 className="text-lg font-semibold font-krona ">
+                        Your cart is empty.
+                    </h1>
+                    <p className="text-gray-400 text-sm ">
+                        You have not added any item to your cart.
+                    </p>
+                    <button
+                        onClick={handleClick}
+                        className="bg-amber-500 text-white px-4 py-2 rounded-md hover:bg-amber-400 mt-4">
+                        Continue Shopping
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default EmptyCart;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,21 +1,29 @@
 import { useSelector } from "react-redux";
 import CartItemBox from "../components/CartItemBox";
 import CartTotal from "../components/CartTotal";
+import EmptyCart from "../components/EmptyCart";
 
 const Cart = () => {
     const { items, total } = useSelector((state) => state.cart);
 
     return (
-        <div className="bg-light-grey mt-16 md:mt-14 py-8 min-h-screen">
-            <div className="container w-full md:w-5/6  mx-auto bg-white rounded-lg shadow-md pt-6">
-                <h3 className="font-krona text-2xl md:text-3xl text-center">
-                    Cart({total})
-                </h3>
-                <div className="grid grid-cols-12 gap-4">
-                    <CartItemBox />
-                    <CartTotal />
+        <div
+            className={`bg-light-grey ${
+                items.length === 0 ? "py-0 h-screen" : "py-8 min-h-screen mt-16 md:mt-14"
+            }`}>
+            {items.length === 0 ? (
+                <EmptyCart />
+            ) : (
+                <div className="container w-full md:w-5/6  mx-auto bg-white rounded-lg shadow-md pt-6">
+                    <h3 className="font-krona text-2xl md:text-3xl text-center">
+                        Cart({total})
+                    </h3>
+                    <div className="grid grid-cols-12 gap-4">
+                        <CartItemBox />
+                        <CartTotal />
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
