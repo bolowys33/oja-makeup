506c744f6985464b76bceaca8c72c337d2509468
Olubodun <bolowys33@gmail.com>
2024-03-25 19:01:49 +0100

    Add responsive design to OrderItem component

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -5,11 +5,11 @@ import timeAgo from "../constants/timeAgo";
 const OrderItem = ({order}) => {
 
     return (
-       <div className="bg-white py-4 px-3 rounded-md font-semibold font-krona text-xs mb-6">
+       <div className="bg-white py-4 px-3 rounded-md font-semibold font-krona text-[12px] md:text-xs mb-6">
         <div className="flex justify-between items-center">
-            <p>Order No: {order.orderRef}</p>
+            <p className="">Order No: {order.orderRef}</p>
             <Link to={`/orders/${order.id}`}
-            className="bg-yellow hover:bg-dark-yellow p-2 rounded-xl text-xs">Show details</Link>
+            className="bg-yellow hover:bg-dark-yellow p-1 md:p-2 rounded-xl text-[10px] md:text-xs">Show details</Link>
         </div>
         <div className="flex justify-end border-y p-2 my-2">
             <div className={`rounded-full h-5 w-5 ${order.status == 'success' ? "bg-lime-600" : order.status == "pending" ? "bg-amber-500" : "bg-red-600"}`}></div>
