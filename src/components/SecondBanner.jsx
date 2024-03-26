5ada130ece3c8f62be6535f1ea7fe0c786c26340
Olubodun <bolowys33@gmail.com>
2024-03-26 14:54:00 +0100

    Update banner

--- src/components/SecondBanner.jsx
+++ src/components/SecondBanner.jsx
@@ -4,16 +4,16 @@ import SectionLink from "./SectionLink";
 
 const SecondBanner = () => {
     return (
-        <div className="bg-pink-300 flex justify-between md:px-20 w-full items-center container mx-auto relative bottom-[-200px]">
+        <div className="bg-pink-300 flex justify-between md:px-20 w-full items-center container mx-auto relative bottom-[-120px]">
             <div className="">
-                <h4 className="font-krona mb-4 font-bold text-2xl">
+                <h4 className="font-krona mb-4 font-bold text-sm md:text-2xl">
                     Be natural, <br />be wise, <br />be you.
                 </h4>
                 <SectionLink path="/about" name="see more"/>
             </div>
             <img
-                className="py-8"
-                width={200}
+                className="py-8 w-[120px] md:w-[200px]"
+                // width={200}
                 src={bannerImg}
                 alt="Tooth brush and paste laying on top of a green leaf"
             />
